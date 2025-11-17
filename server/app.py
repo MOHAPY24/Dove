2bb8b570d7165536b1b848fb0b0af9c0188c9e85
MOHAPY24 <mohammedmahmoud123b@gmail.com>
2025-11-17 19:23:11 +0400

    fixed AUTH

--- server/app.py
+++ server/app.py
@@ -1,5 +1,9 @@
 from recive import app, init
 
 if __name__ == '__main__':
-    init(124)
+    key = input("serv_key (INT only) > ")
+    if key.isalnum == False:
+        print("Non-int key!")
+        quit(1)
+    init(int(key))
     app.run(host="0.0.0.0")
