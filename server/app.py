b46d7c31f943912b7e6abc23446ca86bdaafe3be
MOHAPY24 <mohammedmahmoud123b@gmail.com>
2025-11-17 20:50:02 +0400

    auth fix

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@ from recive import app, init
 
 if __name__ == '__main__':
     key = input("serv_key (INT only) > ")
-    if key.isalnum == False:
+    if key.isalnum() == False:
         print("Non-int key!")
         quit(1)
     init(int(key))
